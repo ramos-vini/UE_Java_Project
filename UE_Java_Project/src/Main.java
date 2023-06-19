a28eb4a16a11285826e7cc287c3738038513c52e
ramos-vini <ramoscontrole@gmail.com>
2023-06-19 20:25:26 +0200

    Basic Order Structure

--- UE_Java_Project/src/Main.java
+++ UE_Java_Project/src/Main.java
@@ -12,34 +12,84 @@ import orders.*;
 public class Main {
 
 	public static void main(String[] args) {
+		// Item available
 		ArrayList<String> itemsList = new ArrayList<String>(Arrays.asList("Desktop", "Laptop", "LCD Screen", "Mouse"));
-		Integer orderId = 1;
-		Integer item;
-		
+
 		System.out.println(" ____________________ ");
-		for(int i = 0; i < itemsList.size(); i++) {
-			System.out.printf("| " + (i+1) + "  -  %-12s%2s\n", itemsList.get(i), "|");
+		for (int i = 0; i < itemsList.size(); i++) {
+			System.out.printf("| " + (i + 1) + "  -  %-12s%2s\n", itemsList.get(i), "|");
 			System.out.println("|____________________|");
 		}
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("");
-		System.out.println("Enter the number of the item: ");
-		
+		// Order creation
+		Order order = new Order();
 
+		// Item Input
+		Boolean addMoreProducts = true;
+		
+		while (addMoreProducts == true) {
+
+			Scanner scanner = new Scanner(System.in);
+
+			// Type
+			Integer item = 0;
+
+			while (item < 1 || item > itemsList.size()) {
+				try {
+					System.out.println("\nEnter the number of the item (1-" + itemsList.size() + "): ");
+					item = scanner.nextInt();
+				} catch (Exception e) {
+					scanner.nextLine();
+				}
+			}
+
+			// Quantity
+			Integer quantity = 0;
+
+			while (quantity < 1) {
+				try {
+					System.out.println("\nEnter the quantity of the item: ");
+					quantity = scanner.nextInt();
+				} catch (Exception e) {
+					scanner.nextLine();
+				}
+			}
+
+			// addItems()
+
+			// Add more products or Finish the Order
+			String addMoreResponse = "";
+			while (addMoreResponse != "Y" || addMoreResponse != "N") {
+				try {
+					System.out.println("\nAdd more products? (Y/N)");
+					addMoreResponse = (scanner.nextLine()).toUpperCase();
+					
+					if(addMoreResponse == "N") {
+						addMoreProducts = false;
+					}
+					
+				} catch (Exception e) {
+					scanner.nextLine();
+				}
+			}
+
+			scanner.close();
+		}
+		
+		// addOrder()
 	}
-	
+
 	// Test static methods
 	public static void addProductDetails() {
-		
+
 	}
-	
+
 	public static void readOrder() {
-		
+
 	}
-	
+
 	public static void printResults() {
-		
+
 	}
 
 }
