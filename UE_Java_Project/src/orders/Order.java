1895461dd62d1faa3af04fad4d5204c3f28a341f
ramos-vini <ramoscontrole@gmail.com>
2023-06-19 20:24:28 +0200

    Order ID removed

--- UE_Java_Project/src/orders/Order.java
+++ UE_Java_Project/src/orders/Order.java
@@ -6,16 +6,9 @@ import items.Item;
 
 public class Order {
 	
-	private Integer id; // Primary Key
 	private ArrayList<Item> items;
 	
 	// Getters & Setters
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
 	
 	public ArrayList<Item> getItems() {
 		return items;
